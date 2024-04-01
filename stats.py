39f3a3eac4505680cfa9561a5f1df6976bd5ed2f
Parth <psomani16k@gmail.com>
2024-04-01 11:36:32 +0530

    rename and wrote stats code

--- stats.py
+++ stats.py
@@ -1,12 +1,34 @@
 import os as os
+import datetime as dt
+from termcolor import colored
 
-dir_path = 'C:/Users/Parth Somani/Desktop/DSA/Problems'
-monthDirs = os.listdir(dir_path)
-
+PROBLEMS_DIRECTORY = os.getcwd() + "\Problems"
 
 def monthlyQuestions():
+    monthDirs = os.listdir(PROBLEMS_DIRECTORY)
+    print("--------------")
     for i in monthDirs:
         count = 0
-        for root_dir, cur_dir, files in os.walk(dir_path + "/" + i):
+        for root_dir, cur_dir, files in os.walk(PROBLEMS_DIRECTORY + "\\" + i):
             count += len(files)
-        print(f"{i}:", count)
+        
+        print(f"{i} :", count)
+        print("--------------")
+
+def daysMissed():
+    checkDate = dt.datetime(2024, 3, 28)
+    while checkDate < dt.datetime.now():
+        dirPath = PROBLEMS_DIRECTORY + "\\" + checkDate.strftime('%m-%Y\%d')
+        if(checkDate.day == 1):
+            print("------------")
+            print(checkDate.strftime("%B-%Y"))
+            print("------------")
+        try:    
+            if len(os.listdir(dirPath)) == 0:
+                print(colored(checkDate.strftime('%d-%m-%y'), 'red'))
+            else:    
+                print(colored(checkDate.strftime('%d-%m-%y'), 'green'))
+        except:
+            print(colored(checkDate.strftime('%d-%m-%y'), 'red'))
+        checkDate = checkDate + dt.timedelta(1)
+
